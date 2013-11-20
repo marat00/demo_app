09d9b77df446be6f48f2af23bc89adc3ac4464f9
marat00 <marat75@gmail.com>
2013-11-20 13:49:40 -0800

    Added a home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 DemoApp::Application.routes.draw do
+  get "welcome/index"
   resources :microposts
   resources :users
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
